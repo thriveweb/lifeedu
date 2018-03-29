3c10a4c060568bb8e4d1dc9a7019dec99bdcbaaa
Alex <alex@thriveweb.com.au>
2018-03-29 16:48:24 +1000

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import BackgroundImage from './BackgroundImage'
 import './Header.css'
 
 export default () => (
   <header className='Header'>
     <div className='relative Header--image container'>
-      <img src='/images/header.png' alt='life' />
+      <img src='/images/header.svg' alt='life' />
     </div>
   </header>
 )
