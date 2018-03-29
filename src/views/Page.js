c1b1ebd3db47636fc961ad35dbca6343185c10bc
Alex <alex@thriveweb.com.au>
2018-03-29 17:00:06 +1000

    getting page ready

--- src/views/Page.js
+++ src/views/Page.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import LazyImage from '../components/LazyImage'
 import Content from '../components/Content.js'
 import './About.css'
 
@@ -13,14 +12,7 @@ export default ({ page }) => (
 
     <div className='section thin'>
       <div className='container'>
-        <Content source={page.section1} />
-      </div>
-    </div>
-    <div className='section thin'>
-      <div className='container'>
-        <Content source={page.section2} />
-        <p>The image below is a {'<LazyImage />'}</p>
-        <LazyImage src={page.featuredImage} alt='LazyImage' />
+        <Content source={page.content} />
       </div>
     </div>
   </div>
