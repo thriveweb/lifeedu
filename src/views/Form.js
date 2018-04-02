f63a362fccbcd389e96afd07031951843c15f292
Alex <alex@thriveweb.com.au>
2018-04-03 09:26:41 +1000

    add form

--- src/views/Form.js
+++ src/views/Form.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import LazyImage from '../components/LazyImage'
 import Content from '../components/Content.js'
-import './About.css'
+import './Form.css'
 
 export default ({ page }) => (
-  <div className='About'>
+  <div className='Form'>
     <Helmet>
       <title>{page.title}</title>
     </Helmet>
     <div className='section thin'>
       <div className='container'>
-        Form
-        <Content source={page.section1} />
-      </div>
-    </div>
-    <div className='section thin'>
-      <div className='container'>
-        <Content source={page.section2} />
-        <p>The image below is a {'<LazyImage />'}</p>
-        <LazyImage src={page.featuredImage} alt='LazyImage' />
+        <Content source={page.content} />
+        {page.audio && (
+          <audio controls>
+            <source src={page.audio} type='audio/mpeg' />
+          </audio>
+        )}
       </div>
     </div>
   </div>
--- src/views/Page.js
+++ src/views/Page.js
@@ -5,11 +5,10 @@ import Content from '../components/Content.js'
 import './Page.css'
 
 export default ({ page }) => (
-  <div className='About'>
+  <div className='Page'>
     <Helmet>
       <title>{page.title}</title>
     </Helmet>
-
     <div className='section thin'>
       <div className='container'>
         <Content source={page.content} />
